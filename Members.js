b3dbcec2ff1ba4bd8934f46f3d1cf9b66fcf2fd1
Deepak Bhandari <Deepbh.forever@gmail.com>
2019-03-05 23:59:06 +0200

    learning 1

---
+++ Members.js
@@ -0,0 +1,14 @@
+
+const members = [
+    {
+        id: 1,
+        name: "this guy",
+        email: "this@gmsil.com"
+    },
+    {
+        id:2,
+        name:"which guy",
+        email:"that@gmail.com"
+    }
+]
+module.exports = members
